09cddff30c7d299d7121fc92a35084906e2c9be2
Adejuwon Ayodimeji <juwwarn28@gmail.com>
2022-03-17 21:08:20 +0100

    Initial Commit

---
+++ ImageGalle/ImageGalleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageGalleApp.swift
+//  ImageGalle
+//
+//  Created by Ayodimeji Adejuwon on 17/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct ImageGalleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
